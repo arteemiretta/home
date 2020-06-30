dd48c328f1e3eeec8c4959c1bddddcb4284dfe59
arteemiretta <67439024+arteemiretta@users.noreply.github.com>
2020-06-30 15:13:43 -0400

    Add files via upload

---
+++ assets/javascript/prices.js
@@ -0,0 +1,29 @@
+var pan_andino = "1.5"; // precio
+var andino_quan = "2";// cantidad
+var pan_dulce = "1"; // precio
+var dulce_quan = "6";// cantidad
+var brioche = "1"; // precio
+var brioche_quan = "5";// cantidad
+var pan_pinita = "1"; // precio
+var pinita_quan = "10";// cantidad
+var cupcakes = "1"; // precio
+var cup_quan = "4";// cantidad
+var pan_chino = "1"; // precio
+var chino_quan = "8";// cantidad
+var rol_canela = "1"; // precio
+var cane_quan = "3";// cantidad
+var pan_oregano = "1"; // precio
+var oregan_quan = "6";// cantidad
+
+
+
+
+    $("#andino").text('VALOR NUEVO');
+    ducument.querySelector('#dawd').innerHTML = 5;
+    ducument.querySelector('#dawd').innerHTML = 5;
+    ducument.querySelector('#dawd').innerHTML = 5;
+    ducument.querySelector('#dawd').innerHTML = 5;
+    ducument.querySelector('#dawd').innerHTML = 5;
+    ducument.querySelector('#dawd').innerHTML = 5;
+    ducument.querySelector('#dawd').innerHTML = 5;
+    ducument.querySelector('#dawd').innerHTML = 5;
---
+++ assets/javascript/require.js
@@ -0,0 +1,30 @@
+function who() {
+    Swal.fire({
+        icon: 'Info',
+        title: '¿Quienes somos?',
+        text: 'Arte Emiretta es una panadería...   Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, illo? Reprehenderit iste, consequatur cumque officia nesciunt sed, minus aliquam sint obcaecati labore dolore animi minima nisi nihil illum similique nemo.',
+        footer: '<p>Gracias por preferirnos</p>'
+      })
+}
+
+$(document).ready(function(){
+ 
+    $('.ir-arriba').click(function(){
+      $('body, html').animate({
+        scrollTop: '0px'
+      }, 300);
+    });
+   
+    $(window).scroll(function(){
+      if( $(this).scrollTop() > 0 ){
+        $('.ir-arriba').slideDown(300);
+      } else {
+        $('.ir-arriba').slideUp(300);
+      }
+    });
+   
+   });
+
+
+
+AOS.init();
